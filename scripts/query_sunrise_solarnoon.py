d445339627af1a2a998b5d17fbd16b3e542f7c0e
Zuokun <zuokun@gmail.com>
2019-12-03 11:29:47 -0500

    八关斋戒时间查询 - 12 months

--- scripts/query_sunrise_solarnoon.py
+++ scripts/query_sunrise_solarnoon.py
@@ -10,8 +10,13 @@ import lxml.html as lh
 import pandas as pd
 import base64
 import pdfkit as pdf
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 def print_sunrise_solarnoon(timeanddate_city_url):
+    print ('--------------------------------------------')
+    print (timeanddate_city_url)
+
     if timeanddate_city_url.find('/'):
         fname = timeanddate_city_url.rsplit('/', 1)[1]
         fname = fname.replace('?','_')
@@ -19,36 +24,36 @@ def print_sunrise_solarnoon(timeanddate_city_url):
     else:
         fname = base64.b64encode(timeanddate_city_url)
 
-    
-    
-    print ('--------------------------------------------')
-    print (timeanddate_city_url)
-    page = requests.get(timeanddate_city_url)
-    df_list = pd.read_html(page.content,attrs = {'id': 'as-monthsun'})
-
-    #print (df_list[0].columns)
-    # astronomy
-    #print (df_list[0].iloc[:, [0,1,5]])
-
-    #sun
-    print (df_list[0].iloc[:, [0,1,11]].to_string())
-    #df_list[0].iloc[:, [0,1,11]].to_html(fname + '.html',index=False)
-
-    html = df_list[0].iloc[:, [0,1,11]].to_html(index=False)
-
     with open(fname + '.html', "w", encoding="utf-8") as file:
         file.writelines('<meta charset="UTF-8">\n')
-        file.write(html)
+
+    for x in range(12):
+        ts_month = date.today() + relativedelta(months=+x)
+        url_month = ts_month.month
+        url_year = ts_month.year
+        # https://www.timeanddate.com/sun/hong-kong/hong-kong?month=12&year=2019
+        full_url = timeanddate_city_url + '?month=' + str(url_month) + '&year=' + str(url_year)
+    
+        print (full_url)
+        page = requests.get(full_url)
+        df_list = pd.read_html(page.content,attrs = {'id': 'as-monthsun'})
+        print (df_list[0].iloc[:, [0,1,11]].to_string())
+    
+        html = df_list[0].iloc[:, [0,1,11]].to_html(index=False)
+        with open(fname + '.html', "a+", encoding="utf-8") as file:
+            file.write(html)
+    
 
     nazivFajla=fname + '.pdf'
     pdf.from_file(fname + '.html', nazivFajla)
     print ('--------------------------------------------')
 
-#print_sunrise_solarnoon('https://www.timeanddate.com/sun/canada/vancouver')
-#print_sunrise_solarnoon('https://www.timeanddate.com/sun/canada/ottawa')
-#print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/beijing')
-#print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/guangzhou')
-#print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/shanghai')
-#print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/chengdu')
-print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/chengdu?month=1&year=2020')
+print_sunrise_solarnoon('https://www.timeanddate.com/sun/canada/vancouver')
+print_sunrise_solarnoon('https://www.timeanddate.com/sun/canada/ottawa')
+print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/beijing')
+print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/guangzhou')
+print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/shanghai')
+print_sunrise_solarnoon('https://www.timeanddate.com/sun/china/chengdu')
+
+print_sunrise_solarnoon('https://www.timeanddate.com/sun/hong-kong/hong-kong')
 
