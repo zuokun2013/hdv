a9ecb447dea574285d3a2de8ea34eec5ed2622b5
zuokun2013 <zuokun@gmail.com>
2020-07-06 23:12:12 -0700

    jtly

--- bin/md2pdf.sh
+++ bin/md2pdf.sh
@@ -6,6 +6,7 @@ pandoc $1 -f markdown -s \
     -V 'geometry:a6paper' \
     -V 'geometry:margin=1.2cm' \
     --pdf-engine=xelatex --toc --number-section  \
+    -H $(dirname $0)/options.sty \
     -o $1.pdf
 
 echo "--- `date` $1 done. ---" 
