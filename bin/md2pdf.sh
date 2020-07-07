eced43be27ad514ec44fb34d297569a47864855c
zuokun2013 <zuokun@gmail.com>
2020-07-07 13:47:42 -0700

    script tweak

--- bin/md2pdf.sh
+++ bin/md2pdf.sh
@@ -2,9 +2,6 @@ echo "--- `date` $1 ---"
 pandoc $1 -f markdown -s \
     -V 'mainfont:DejaVu Sans' \
     -V 'CJKmainfont:Yuanti SC' \
-    -V 'linkcolor:blue' \
-    -V 'geometry:a6paper' \
-    -V 'geometry:margin=1.5cm' \
     --pdf-engine=xelatex --toc --number-section  \
     -H $(dirname $0)/options.sty \
     -o $1.pdf
