882604b1399c226e938c0f57160e8622c388686b
zuokun2013 <zuokun@gmail.com>
2020-11-12 18:24:33 -0800

    mirror script

---
+++ bin/mirror-hdsite.sh
@@ -0,0 +1,4 @@
+
+wget --mirror --page-requisites --adjust-extension --span-hosts -nc -N --no-if-modified-since --convert-links --domains www.huidengzhiguang.com,d.huidengzhiguang.com -e robots=off -U 'M
+ozilla/5.0 (X11; Ubuntu;Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0' http://www.huidengzhiguang.com 
+
