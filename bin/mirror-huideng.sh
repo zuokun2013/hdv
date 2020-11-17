4acbac135c9708ad3b125bb02f2d3812fc63e94c
zuokun2013 <zuokun@gmail.com>
2020-11-16 19:21:06 -0800

    wget miror site scripts

---
+++ bin/mirror-huideng.sh
@@ -0,0 +1,3 @@
+
+wget --mirror --page-requisites --adjust-extension --span-hosts -N --no-if-modified-since --convert-links --domains huidengzhiguang.com,huidengzhiguang.net -e robots=off -U 'Mozilla/5.0 (X11; Ubuntu;Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0' http://www.huidengzhiguang.com 
+
---
+++ bin/mirror-xianmifw.sh
@@ -0,0 +1,2 @@
+cat xmfw_wget.sh 
+wget --mirror --page-requisites --adjust-extension --span-hosts -N --no-if-modified-since --convert-links --domains xianmifw.com -e robots=off -U 'Mozilla/5.0 (X11; Ubuntu;Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0' http://www.xianmifw.com
