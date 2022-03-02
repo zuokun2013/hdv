0200113a7b9e5327962553862141a7dee2a4e579
zuokun2013 <zuokun@gmail.com>
2022-03-02 13:22:04 -0800

    Add files via upload

---
+++ bin/wget-download-wechat-pages.sh
@@ -0,0 +1,5 @@
+#  慈诚罗珠堪布慧灯之光系列 微信公众号备份
+# 为响应相关规定，此平台（慈诚罗珠堪布慧灯之光系列）将于2022年3月1日转型
+
+wget.exe -e robots=off --recursive --level=2  --page-requisites --adjust-extension –-no-parent --span-hosts -N --convert-links -U "Mozilla/5.0 (X11; Ubuntu;Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0" --no-check-certificate --restrict-file-names=ascii --domains mp.weixin.qq.com,mmbiz.qpic.cn "https://mp.weixin.qq.com/s/MQJ68nuhejdgPaRD0qbj9w"
+
