4623246c9ee7199bd5c563d10fffd5b44afa2772
zuokun2013 <zuokun@gmail.com>
2022-04-04 20:00:02 -0700

    Add files via upload

---
+++ bin/gen-mp4/play-only.py
@@ -0,0 +1,54 @@
+import os
+import time
+import tkinter as tk
+from random import choice, seed
+from string import ascii_letters
+from threading import *
+
+from pysubparser import parser
+
+seed(42)
+
+root = tk.Tk()
+
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(),
+                                   root.winfo_screenheight()))
+#root.attributes('-fullscreen', True)
+root.state('zoomed')
+# root.wm_overrideredirect(True)
+# root.bind("<Button-1>", lambda evt: root.destroy())
+
+l = tk.Label(text='', font=("SimHei", 60), fg='blue')
+l.pack(expand=True)
+
+subtitles = parser.parse('xiaman-01.kdenlive.srt')
+
+
+def work():
+
+    i = 0
+    for subtitle in (subtitles):
+        print(f'{subtitle.start} > {subtitle.end}')
+        print(subtitle.duration / 1000)
+        i = i + 1
+
+        sbtt = """
+        {}
+        """.format("\n".join(subtitle.lines[0:])) + "\n(" + str(i) + ")"
+
+        print(sbtt.lstrip())
+        l.config(text=sbtt.lstrip())
+        os.system(
+            f'ffplay -nodisp -ss {subtitle.start}  -t {subtitle.duration / 1000} -autoexit  "《心性休息》颂词 朗诵版01.mp3"'
+        )
+        print()
+
+        time.sleep(3)
+
+
+# work function
+t1 = Thread(target=work)
+t1.start()
+
+l.config(text='')
+root.mainloop()
