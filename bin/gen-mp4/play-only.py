5397616d3b22c1313a33e8b2f2248c57c14b4458
zuokun2013 <zuokun@gmail.com>
2022-04-07 19:45:05 -0700

    Update play-only.py

--- bin/gen-mp4/play-only.py
+++ bin/gen-mp4/play-only.py
@@ -4,8 +4,12 @@ import tkinter as tk
 from random import choice, seed
 from string import ascii_letters
 from threading import *
+from tracemalloc import start
 
 from pysubparser import parser
+
+start_pos = 10
+pause_time = 60
 
 seed(42)
 
@@ -23,25 +27,34 @@ root.configure(bg='blue')
 l = tk.Label(text='', font=("STLITI", 60), fg='white',bg='blue')
 l.pack(expand=True)
 dir_path = os.path.dirname(os.path.realpath(__file__)) + '\\'
-subtitles = parser.parse( dir_path +'xiaman-01.kdenlive.srt')
-total_st = len(list(subtitles))
-subtitles = parser.parse( dir_path +'xiaman-01.kdenlive.srt')
+tmp_subtitles = list(parser.parse( dir_path +'xiaman-01.kdenlive.srt'))
+#total_st = len(subtitles)
+#subtitles = parser.parse( dir_path +'xiaman-01.kdenlive.srt')
+
+
+subtitles = []
+subtitles.extend(tmp_subtitles[start_pos:len(tmp_subtitles)])
+subtitles.extend(tmp_subtitles[0:start_pos])
 
 def work():
 
     i = 0
     for subtitle in (subtitles):
+        i = i + 1
+        
+        print(subtitle)
         print(f'{subtitle.start} > {subtitle.end}')
         print(subtitle.duration / 1000)
-        i = i + 1
+       
 
         sbtt = """
         {}
-        """.format("\n".join(subtitle.lines[0:])) + "\n(" + str(i) + "/" + str(total_st) + ")"
+        """.format("\n".join(subtitle.lines[0:])) + "\n(" + str(tmp_subtitles.index(subtitle)) + "/" + str(len(subtitles)) + ")"
 
         print(sbtt.lstrip())
         l.config(text=sbtt.lstrip())
         print()
+        
 
         os.system(
             f'ffplay -nodisp -autoexit "' + dir_path + 'ding.mp3"'
@@ -49,7 +62,7 @@ def work():
         os.system(
             f'ffplay -nodisp -ss {subtitle.start}  -t {subtitle.duration / 1000} -autoexit  "' + dir_path + '《心性休息》颂词 朗诵版01.mp3"'
         )        
-        time.sleep(60)
+        time.sleep(pause_time)
 
 
 # work function
