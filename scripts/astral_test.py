a7d99ebdc811d5a3e53f18e60b264a73b13ec19b
YuanHangP50 <zuokun@gmail.com>
2022-05-14 23:23:00 -0700

    cheng liang pin wip

--- scripts/astral_test.py
+++ scripts/astral_test.py
@@ -9,10 +9,10 @@ from astral import LocationInfo
 
 for timezone in all_timezones:
     print(timezone)
-
+# 49.160778,-123.1459327
 city = LocationInfo("Richmond", "Canada", "Canada/Pacific",
-                    49.16072096422805, -123.14415705501548)
-s = sun(city.observer, date=datetime.date(2021, 12, 13), tzinfo=city.timezone)
+                    49.16072096422805, -123.1459327)
+s = sun(city.observer, date=datetime.date(2022, 5, 15), tzinfo=city.timezone)
 print((
     f'日出: {s["sunrise"]}\n'
     f'正午: {s["noon"]}\n'
