f4d6ff35661608f65bb3dd57c39b1476f7326ce5
zuokun <zuokun@gmail.com>
2022-11-27 08:32:43 -0800

    mpv cmd file for zaoke

---
+++ media/早课v2.cmd
@@ -0,0 +1,13 @@
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\大恩上师-加倍咒.ogg
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\大恩上师-念珠加持咒.ogg
+mpv --speed=1.33 C:\git-repos\hdv\media\语加持\yjc-2022.mp3
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\大恩上师-圣八吉祥.ogg
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\金刚萨埵如意宝珠及百字明21遍.ogg
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\大恩上师-课前念诵.ogg
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\大恩上师-上师瑜伽速赐加持.ogg
+C:\Users\zuokun\Videos\寿命无常.mp4
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\方磊及万佛圣城-楞严咒及咒心.ogg
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\清净明悔1_2022-04-17_14-36-20.ogg
+mpv --speed=1.33 C:\git-repos\hdv\media\念诵\法王如意宝-怀业祈祷文.ogg
+mpv --speed=1.2 C:\git-repos\hdv\media\念诵\灵岩山寺女众师父-大悲咒及心经.ogg
+mpv --speed=1.5 C:\Users\zuokun\Music\观音上师-观音心咒110遍-1min.mp3
