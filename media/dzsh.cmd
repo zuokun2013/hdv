04a26c245b8eb40849f5053e91df8abcb3ca24e7
Yuan Hang <zuokun@gmail.com>
2023-01-06 19:55:57 -0800

    dzj wlsj

--- media/dzsh.cmd
+++ media/dzsh.cmd
@@ -1,12 +1,12 @@
 chcp 65001
 cd C:\git-repos\hdv\media\
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
-mpv .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
+mpv --speed=0.9 .\110ding2mins.m4a
