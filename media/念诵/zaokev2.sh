2d794f79f3c9085f6f8f83f86e11395241181d6a
jzhang <jzhang@jzhangs-MacBook-Pro.local>
2023-02-22 15:12:20 -0800

    nian song

---
+++ media/念诵/zaokev2.sh
@@ -0,0 +1,13 @@
+
+mpv --speed=1.33 ./大恩上师-加倍咒.ogg
+mpv --speed=1.33 ./大恩上师-念珠加持咒.ogg
+mpv --speed=1.33 ../语加持/yjc-2022.mp3
+mpv --speed=1.33 ./大恩上师-圣八吉祥.ogg
+mpv --speed=1.33 ./金刚萨埵如意宝珠及百字明21遍.ogg
+mpv --speed=1.33 ./大恩上师-课前念诵.ogg
+mpv --speed=1.33 ./大恩上师-上师瑜伽速赐加持.ogg
+
+mpv --speed=1.2 ./方磊及万佛圣城-楞严咒及咒心.ogg
+mpv --speed=1.33 ./清净明悔1_2022-04-17_14-36-20.ogg
+mpv --speed=1.33 ./法王如意宝-怀业祈祷文.ogg
+mpv --speed=1.2 ./灵岩山寺女众师父-大悲咒及心经.ogg
