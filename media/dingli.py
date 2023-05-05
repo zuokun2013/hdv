067364478af463b4a3ae178498dd0a20405e7aae
Yuan Hang <zuokun@gmail.com>
2023-05-04 20:47:01 -0700

    play files using python

--- media/dingli.py
+++ media/dingli.py
@@ -5,4 +5,4 @@ print(realpath)
 os.system('cd ' + realpath)
 
 os.system('mpv "2023-35佛忏悔文.mp3"')
-os.system('mpv "法王如意宝上师瑜伽110遍.mp3"')
+os.system('mpv --speed=0.9 "法王如意宝上师瑜伽110遍.mp3"')
--- media/shushengri-gongxiu.py
+++ media/shushengri-gongxiu.py
@@ -5,17 +5,24 @@ realpath=os.path.abspath(__file__)
 print(realpath)
 os.system('cd ' + realpath)
 
+os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/a/恒常念诵愿文.mp3"')
+
 os.system('mpv --fullscreen "https://f.huidengchanxiu.net/hdv/videos/%E8%AF%BE%E5%89%8D%E5%BF%B5%E8%AF%B5.mp4" ')
 os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/%e9%87%91%e5%88%9a%e4%b8%83%e5%8f%a5-%e6%b5%81%e7%95%85%e7%89%88.mp4"')
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/%e6%99%ae%e8%b4%a4%e5%a6%82%e6%9d%a5%e6%84%bf%e6%96%87.mp4"')
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/%e6%99%ae%e8%b4%a4%e5%a6%82%e6%9d%a5%e6%84%bf%e6%96%87.mp4"')
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/%e6%99%ae%e8%b4%a4%e5%a6%82%e6%9d%a5%e6%84%bf%e6%96%87.mp4"')
+os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+time.sleep(3)
+os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+time.sleep(3)
+os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+time.sleep(3)
+
 os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/v/4jx/%E6%9A%87%E6%BB%A1%E9%9A%BE%E5%BE%97-%E4%B8%8A%E5%B8%88%E5%BF%B5%E8%AF%B5.mp4"')
 
+os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/v/法王如意宝阿弥陀佛灌顶实录.mp4"')
 # sleep 5 secs
 time.sleep(300)
 
 os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/yigui/%e8%af%b8%e4%bd%9b%e8%8f%a9%e8%90%a8%e5%90%8d%e5%8f%b7%e9%9b%86-%e5%bf%b5%e8%af%b5%e4%bb%aa%e8%bd%a8.mp4"')
 os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/%E5%9B%9E%E5%90%91(2021%E7%89%88).mp4"')
-# os.system('mpv --fullscreen  ""')
+# os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/a/%e6%81%92%e5%b8%b8%e5%bf%b5%e8%af%b5%e6%84%bf%e6%96%87.mp3"')
 # os.system('mpv --fullscreen  ""')
