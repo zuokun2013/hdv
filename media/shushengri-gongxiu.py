3b001d5e0e42c29f0e3522e5d686ddb6e477d0f8
Yuan Hang <zuokun@gmail.com>
2023-05-04 23:10:24 -0700

    shushengri gongxiu download then play

--- media/shushengri-gongxiu.py
+++ media/shushengri-gongxiu.py
@@ -3,26 +3,48 @@ import time
  
 realpath=os.path.abspath(__file__)
 print(realpath)
-os.system('cd ' + realpath)
+# os.system('cd ' + realpath)
 
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/a/恒常念诵愿文.mp3"')
+# download files first
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/a/恒常念诵愿文.mp3"')
 
-os.system('mpv --fullscreen "https://f.huidengchanxiu.net/hdv/videos/%E8%AF%BE%E5%89%8D%E5%BF%B5%E8%AF%B5.mp4" ')
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/%e9%87%91%e5%88%9a%e4%b8%83%e5%8f%a5-%e6%b5%81%e7%95%85%e7%89%88.mp4"')
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/videos/课前念诵.mp4" ')
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/videos/金刚七句-流畅版.mp4"')
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+
+
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/v/4jx/暇满难得-上师念诵.mp4"')
+
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/v/法王如意宝阿弥陀佛灌顶实录.mp4"')
+
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/yigui/诸佛菩萨名号集-念诵仪轨.mp4"')
+os.system('wget -N  "https://f.huidengchanxiu.net/hdv/videos/回向(2021版).mp4"')
+
+# 2 play files
+
+os.system('mpv --fullscreen  "恒常念诵愿文.mp3"')
+
+os.system('mpv --fullscreen  "课前念诵.mp4" ')
+os.system('mpv --fullscreen  "金刚七句-流畅版.mp4"')
+os.system('mpv --fullscreen  "普贤如来愿文.mp4"')
 time.sleep(3)
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+os.system('mpv --fullscreen  "普贤如来愿文.mp4"')
 time.sleep(3)
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/普贤如来愿文.mp4"')
+os.system('mpv --fullscreen  "普贤如来愿文.mp4"')
 time.sleep(3)
 
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/v/4jx/%E6%9A%87%E6%BB%A1%E9%9A%BE%E5%BE%97-%E4%B8%8A%E5%B8%88%E5%BF%B5%E8%AF%B5.mp4"')
+os.system('mpv --fullscreen  "暇满难得-上师念诵.mp4"')
 
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/v/法王如意宝阿弥陀佛灌顶实录.mp4"')
-# sleep 5 secs
-time.sleep(300)
+os.system('mpv --fullscreen  "法王如意宝阿弥陀佛灌顶实录.mp4"')
+# sleep 300 secs
+# time.sleep(300)
 
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/yigui/%e8%af%b8%e4%bd%9b%e8%8f%a9%e8%90%a8%e5%90%8d%e5%8f%b7%e9%9b%86-%e5%bf%b5%e8%af%b5%e4%bb%aa%e8%bd%a8.mp4"')
-os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/videos/%E5%9B%9E%E5%90%91(2021%E7%89%88).mp4"')
+os.system('mpv --fullscreen  "诸佛菩萨名号集-念诵仪轨.mp4"')
+os.system('mpv --fullscreen  "回向(2021版).mp4"')
+
 # os.system('mpv --fullscreen  "https://f.huidengchanxiu.net/hdv/a/%e6%81%92%e5%b8%b8%e5%bf%b5%e8%af%b5%e6%84%bf%e6%96%87.mp3"')
 # os.system('mpv --fullscreen  ""')
