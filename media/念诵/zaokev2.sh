1ae5715844763b1c24b20c56e8f20002dc6b873b
Yuan Hang <zuokun@gmail.com>
2023-08-23 07:25:55 -0700

    zao ke shell script

--- media/念诵/zaokev2.sh
+++ media/念诵/zaokev2.sh
@@ -10,8 +10,8 @@ mpv --speed=1.33 $SCRIPTPATH/金刚萨埵如意宝珠及百字明21遍.ogg
 mpv --speed=1.33 $SCRIPTPATH/大恩上师-课前念诵.ogg
 mpv --speed=1.33 $SCRIPTPATH/大恩上师-上师瑜伽速赐加持.ogg
 
-mpv --speed=1.2 $SCRIPTPATH/方磊及万佛圣城-楞严咒及咒心.ogg
+mpv --speed=1.4 $SCRIPTPATH/方磊及万佛圣城-楞严咒及咒心.ogg
 mpv --speed=1.33 $SCRIPTPATH/清净明悔1_2022-04-17_14-36-20.ogg
 mpv --speed=1.33 $SCRIPTPATH/法王如意宝-怀业祈祷文.ogg
 mpv --speed=1.2 $SCRIPTPATH/灵岩山寺女众师父-大悲咒及心经.ogg
-mpv $SCRIPTPATH/每日短咒语念诵.mp3
+mpv --speed=1.3 $SCRIPTPATH/每日短咒语念诵.mp3
